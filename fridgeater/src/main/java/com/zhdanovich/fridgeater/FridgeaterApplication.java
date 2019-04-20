da4f825aaaad88d8ea7e114c7bd5f79cb20eb00c
Sergey <beda67@mail.ru>
2019-04-20 19:19:26 +0300

    Add files via upload

---
+++ fridgeater/src/main/java/com/zhdanovich/fridgeater/FridgeaterApplication.java
@@ -0,0 +1,13 @@
+package com.zhdanovich.fridgeater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridgeaterApplication.class, args);
+	}
+
+}
