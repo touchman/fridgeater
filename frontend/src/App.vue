6e83faa0e92a6df674fc11586356eca8376b0a72
touchman <beda67@mail.ru>
2019-05-19 20:38:49 +0300

    Frontend, vue starting page

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template id="app">
+    <div>
+        <Header/>
+        <PersonList/>
+    </div>
+</template>
+
+<script>
+    import PersonList from "./components/PersonList";
+    import Header from "./components/Header";
+
+    export default {
+        name: 'app',
+        components: {
+            Header,
+            PersonList
+        }
+    }
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
---
+++ frontend/src/components/PersonList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Type</th>
+                <th scope="col">Active</th>
+                <th scope="col">Name</th>
+                <th scope="col">Language</th>
+                <th scope="col">Products</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(recipe, index) in recipes.allRecipes" :key="index">
+                <td>{{recipe.id}}</td>
+                <td>{{recipe.type}}</td>
+                <td>{{recipe.active}}</td>
+                <td>{{recipe.name}}</td>
+                <td>{{recipe.lang}}</td>
+                <td>{{recipe.productList}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        data() {
+            return {
+                recipes: []
+            }
+        },
+
+        created() {
+            axios.get(`/backend/recipe/recipes`)
+                .then(response => {
+                    this.recipes = response.data;
+                });
+        }
+    }
+</script>
+//*[@id="content"]/div[1]/div/div/div
+<style scoped>
+
+</style>
